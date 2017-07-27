8766ad509e98a6b471620d77f379f5d49d5d58f4
rahul@awkword.in <rahul@awkword.in>
2017-07-27 13:10:54 +0530

    Views updated

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -1,5 +1,5 @@
-<div class="modal {{$bootmodal->getOption('animation')? 'fade':''}}" id="{{$bootmodal->getOption('id')}}" tabindex="-1" role="dialog" aria-labelledby="{{$bootmodal->getOption('id')}}_label">
-	<div class="modal-dialog modal-{{$bootmodal->getOption('size')}}" role="document">
+<div class="modal {{$laramodal->getOption('animation')? 'fade':''}}" id="{{$laramodal->getOption('id')}}" tabindex="-1" role="dialog" aria-labelledby="{{$laramodal->getOption('id')}}_label">
+	<div class="modal-dialog modal-{{$laramodal->getOption('size')}}" role="document">
 
 		<div class="modal-content">
 
@@ -8,10 +8,10 @@
 			@else
 
 
-				@if($bootmodal->getOption('title'))
+				@if($laramodal->getOption('title'))
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" >{{$bootmodal->getOption('title')}}</h4>
+						<h4 class="modal-title" >{{$laramodal->getOption('title')}}</h4>
 					</div>
 				@endif
 				
@@ -20,7 +20,7 @@
 				</div>
 				
 				<div class="modal-footer">
-					@if($bootmodal->getOption('dismiss'))
+					@if($laramodal->getOption('dismiss'))
 						<button type="button" class="btn btn-default" data-dismiss="modal">@lang('modal.close')</button>
 					@endif
 
